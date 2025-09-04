e849fe64a4176a87b6c5c1d15e31eee718cc3ebc
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2025-09-03 19:45:37 -0500

    Add example control and some commands

--- finopspp/__main__.py
+++ finopspp/__main__.py
@@ -1,16 +1,57 @@
+import os
+from importlib.resources import files
+
 import click
+import yaml
 
 @click.group()
 def cli():
     pass
 
-@click.group()
-def assessment():
+@cli.group()
+def assessments():
     pass
 
-@assessment.group()
+@assessments.command()
 def generate():
     pass
+
+@cli.group()
+def controls():
+    """Informational command on Controls"""
+    pass
+
+@controls.command()
+def list():
+    """List all Controls by ID"""
+    for file in files('finopspp.controls').iterdir():
+        root, _ = os.path.splitext(file.name)
+        click.echo(root)
+
+@controls.command()
+@click.option(
+    "--metadata",
+    is_flag=True,
+    help='Show the Metadata for a Control.',
+)
+@click.argument('name')
+def show(name, metadata):
+    """Show information on a given Control"""
+    data_type = 'Control'
+    if metadata:
+        data_type = 'Metadata'
+
+    control_file = files('finopspp.controls').joinpath(f'{name}.yaml')
+    click.echo(control_file)
+    with open(control_file, 'r') as file:
+        control_data = yaml.safe_load(file)
+        click.echo(
+            yaml.dump(
+                control_data[data_type],
+                default_flow_style=False,
+                indent=4
+            )
+        )
 
 if __name__ == "__main__":
     cli()
