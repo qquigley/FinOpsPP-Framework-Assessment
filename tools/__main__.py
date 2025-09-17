5dcf75d6314ba90047b6079e5e5a3dd636117922
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2025-09-17 00:25:56 -0500

    Add basic spec files
    for profiles, domains, capabilities, and actions

--- tools/__main__.py
+++ tools/__main__.py
@@ -4,6 +4,22 @@ from importlib.resources import files
 import click
 import yaml
 
+PROFILES_MAP = {}
+def profiles():
+    """Return all profiles. Including proposed one"""
+    global PROFILES_MAP
+    if PROFILES_MAP:
+        return PROFILES_MAP
+
+    profiles = files('finopspp.specifications.profiles')
+    for file in profiles.iterdir():
+        path = profiles.joinpath(file.name)
+        with open(path, 'r') as yaml_file:
+            title = yaml.safe_load(yaml_file).get('Specification').get('Title')
+            PROFILES_MAP[title] = path
+    
+    return PROFILES_MAP
+
 @click.group()
 def cli():
     """FinOps++ administration tool"""
@@ -17,9 +33,9 @@ def generate():
 @generate.command()
 @click.option(
     '--profile',
-    default='standard',
-    type=click.Choice(['standard']), # TODO: probably want to pull the choice from some approved list
-    help='Which assessment profile to generate. Defaults to standard profile',
+    default='FinOps++',
+    type=click.Choice(list(profiles().keys())),
+    help='Which assessment profile to generate. Defaults to "FinOps++',
 )
 @click.option(
     '--name',
@@ -41,11 +57,23 @@ def specifications():
     pass
 
 @specifications.command()
-def list():
-    """List all Specifications by ID"""
-    for file in files('finopspp.specifications').iterdir():
-        root, _ = os.path.splitext(file.name)
-        click.echo(root)
+@click.option(
+    '--profile',
+    default='FinOps++',
+    type=click.Choice(list(profiles().keys())),
+    help='Which assessment profile to use. Defaults to "FinOps++"',
+)
+def list(profile):
+    """List all Specifications by fully qualified ID per profile"""
+    with open(PROFILES_MAP[profile], 'r') as yaml_file:
+        domains = yaml.load(
+            yaml_file, Loader=yaml.BaseLoader
+        ).get('Specification').get('Domains')
+
+    domain_files = files('finopspp.specifications.domains')
+    for domain in domains:
+        number = domain.get('Number')
+        click.echo(domain_files.joinpath(f'{number}.yaml'))
 
 @specifications.command()
 @click.option(
@@ -53,15 +81,22 @@ def list():
     is_flag=True,
     help='Show the Metadata for a Specifications',
 )
+@click.option(
+    '--specification-type',
+    type=click.Choice(['profiles', 'domains', 'capabilities', 'actions']),
+    help='Which specification type to show. Defaults to "profiles"',
+)
 @click.argument('id')
-def show(id, metadata):
-    """Show information on a given specification by ID"""
+def show(id, metadata, specification_type):
+    """Show information on a given specification by ID by type"""
     data_type = 'Specification'
     if metadata:
         data_type = 'Metadata'
 
-    specification_file = files('finopspp.specifications').joinpath(f'{id}.yaml')
-    click.echo(specification_file)
+    specification_file = files(
+        f'finopspp.specifications.{specification_type}'
+    ).joinpath(f'{id}.yaml')
+
     with open(specification_file, 'r') as file:
         specification_data = yaml.safe_load(file)
         click.echo(
